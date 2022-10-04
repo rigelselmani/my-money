fc70fd2caf5a92c99e041a510f3425dfdbac7e6f
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-04 09:40:22 -0400

    login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,8 +2,21 @@ import Styles from "./Login.module.css";
 
 export default function Login(){
     return(
-        <div>
-            Login
-        </div>
+        <form className={Styles["login-form"]}>
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                   type="email"
+                />
+            </label>
+            <label>
+               <span>Password:</span>
+               <input
+                 type="password"
+               />
+            </label>
+            <button className="btn">Login</button>
+        </form>
     )
 } 
