999f989c33f01e6176f68a26dc8dc2ac340ff4be
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-04 09:50:30 -0400

    useState and on change events and submit event

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,19 +1,36 @@
+import { useState } from "react";
+
 import Styles from "./Login.module.css";
 
+
 export default function Login(){
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+
+   const handleClick=(e)=>{
+    e.preventDefault()
+      console.log(email, password)
+   }
     return(
-        <form className={Styles["login-form"]}>
+        <form className={Styles["login-form"]} onSubmit={handleClick}>
             <h2>Login</h2>
             <label>
                 <span>Email:</span>
                 <input 
                    type="email"
+                   onChange={(e) => setEmail(e.target.value)}
+                   value={email}
+
                 />
             </label>
             <label>
                <span>Password:</span>
                <input
                  type="password"
+                 onChange={(e)=> setPassword(e.target.value)}
+                 value={password}
                />
             </label>
             <button className="btn">Login</button>
