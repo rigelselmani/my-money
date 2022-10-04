988d3f863bec8ba28fff9cfa16012347764563a3
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-04 12:13:17 -0400

    useAuthcontext

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import {projectAuth} from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup=()=>{
    const [error, setError]=useState(null);
    const [isePending,setIsPending]=useState(null);
+   const {dispatch} = useAuthContext()
 
    const signup= async(email, password, displayName)=>{
      setError(null)
@@ -12,7 +14,7 @@ export const useSignup=()=>{
      try {
         //signup user
         const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-        console.log(res.user)
+      
         if(!res){
             throw new Error("Could not complete signup")
         }
@@ -20,6 +22,9 @@ export const useSignup=()=>{
         // add display name to user
         await res.user.updateProfile({displayName:displayName})
     
+        // dispatch login action
+      dispatch({type: "LOGIN", payload:res.user})
+
         setIsPending(false)
         setError(null)
     }
