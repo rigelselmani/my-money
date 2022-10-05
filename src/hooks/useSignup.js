4f2aeae47e83868a9e316814226caffabf6d9087
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-05 10:21:49 -0400

    useEffect

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import {projectAuth} from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup=()=>{
+   const [isCancelled, setIsCancelled]=useState(false)
    const [error, setError]=useState(null);
    const [isePending,setIsPending]=useState(null);
    const {dispatch} = useAuthContext()
@@ -20,20 +21,27 @@ export const useSignup=()=>{
         }
 
         // add display name to user
-        await res.user.updateProfile({displayName:displayName})
+        await res.user.updateProfile({displayName})
     
         // dispatch login action
       dispatch({type: "LOGIN", payload:res.user})
-
-        setIsPending(false)
-        setError(null)
+      if(!isCancelled){
+         setIsPending(false)
+         setError(null)
+       }
     }
      catch (err){
-        console.log(err.message)
-        setError(err.message)
-        setIsPending(false)
+      if(!isCancelled){
+         console.log(err.message)
+         setError(err.message)
+         setIsPending(false)
+      }
      }
    }
+
+   useEffect(() => {
+      return()=> setIsCancelled(true)
+   }, [])
 
    return {error, isePending, signup}
 }
