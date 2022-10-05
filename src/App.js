bf66e0f233815fbadb7f960d4205a077950f55d7
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-05 12:09:31 -0400

    redirecting pages if logedin redirect to homepage if not to login or signup page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages& components
@@ -8,7 +8,7 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 function App() {
-   const {authIsReady}=useAuthContext()
+   const {authIsReady,user}=useAuthContext()
   return (
     <div className="App">
     {authIsReady&&(
@@ -16,13 +16,16 @@ function App() {
        <Navbar/>
        <Switch>
            <Route exact path="/">
-              <Home/>
+           {!user&& <Redirect to="/login"/>}
+              {user&&<Home/>}
            </Route>
            <Route path="/login">
-              <Login/>
+           {user && <Redirect to="/"/>}
+              {!user&&<Login/>}
            </Route>
            <Route path="/signup">
-              <Signup />
+              {user && <Redirect to="/" />}
+              {!user &&<Signup />}
            </Route>
        </Switch>
        </BrowserRouter>       
