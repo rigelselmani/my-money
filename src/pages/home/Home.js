768e33c003a707d26436f3858eb53e3f2a1dd61e
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-05 12:40:30 -0400

    transaction component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,17 @@
 import styles from "./Home.module.css";
 
+//components
+import TransactionForm from "./Transaction.Form";
+
 export default function Home (){
     return (
-        <div>
-            Home
+        <div className={styles.container}>
+           <div className={styles.content}>
+            transaction list
+           </div>
+           <div className={styles.sidebar}>
+            <TransactionForm />
+           </div>
         </div>
     )
 }
