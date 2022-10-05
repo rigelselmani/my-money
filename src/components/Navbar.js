e3148bda8740a88a0e3b7e2cfc8e6bd49c4d12cc
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-05 11:17:15 -0400

    hiding and showing the signup , login and logout buttons on the navbar depending on the users status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom";
-import {useLogout} from "../hooks/useLogout"
+import {useLogout} from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 import styles from "./Navbar.module.css";
 
 export default function Navbar(){
     const {logout}=useLogout()
+    const {user} = useAuthContext()
 
     return (
        <nav className={styles.navbar}>
           <ul>
             <li className={styles.title}>myMoney</li>
+            {!user&&
+            <>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
-
+            </>
+            }
+            {user&&(
+            <> 
+            <li>hello, {user.displayName}</li>
             <li>
-             <button className="btn" onClick={logout}>Logout</button>
+              <button className="btn" onClick={logout}>Logout</button>
             </li>
+            </>  
+            )}
           </ul>
        </nav>
     )
