c3022ff38efe0c451f4ddd64117670f9ac5fa76d
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-11 18:32:13 -0400

    add  document and delete document functions

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,6 +10,12 @@ let initialState ={
 
 const firestoreReducer = (state, action)=>{
     switch (action.type){
+        case "IS_PENDING":
+        return {isPending: true, document:null,success:false, error:null} 
+        case "ADDED_DOCUMENT":
+            return {isPending:false, document:action.payload, success:true, error:null}
+              case "ERROR":
+                return{isPending:false, document:null, success:false,error:action.payload}  
         default:
             return state
     }
@@ -22,13 +28,28 @@ export const useFirestore = (collection)=>{
     //collection ref
     const ref= projectFirestore.collection(collection)
 
-    //add document
-    const addDocument = (doc)=>{
+    //only dispatch is not callcelled
+const dispatchIfNotCancelled = (action)=>{
+    if(!isCancelled){
+        dispatch(action)
+    }
+}
 
+    //add document
+    const addDocument = async(doc)=>{
+       dispatch({type: "IS_PENDING"})
+
+       try {
+        const addedDocument = await ref.add(doc)
+        dispatchIfNotCancelled({type: "ADDED_DOCUMENT", payload: addedDocument})
+       }
+       catch (err){
+        dispatchIfNotCancelled({type:"ERROR", payload: err.message})
+       }
     }
 
     //delete document
-    const deleteDocument =(id)=>{
+    const deleteDocument =async(id)=>{
 
     }
 
