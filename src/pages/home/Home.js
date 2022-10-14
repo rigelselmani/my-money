fd88dc45b0d99b35969868303b68efb8c5660c30
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-14 13:08:19 -0400

    sending the data items to firestore and connecting with the userid

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,19 @@
+import {useAuthContext} from "../../hooks/useAuthContext";
+
 import styles from "./Home.module.css";
 
 //components
 import TransactionForm from "./Transaction.Form";
 
 export default function Home (){
+    const {user}=useAuthContext()
     return (
         <div className={styles.container}>
            <div className={styles.content}>
             transaction list
            </div>
            <div className={styles.sidebar}>
-            <TransactionForm />
+            <TransactionForm uid={user.uid}/>
            </div>
         </div>
     )
